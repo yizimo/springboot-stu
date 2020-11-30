f461dd05a91ebdb538df5fdfb137c40ce58d5c09
zimo <1040099868@qq.com>
2020-11-30 09:20:56 +0800

    创建项目

---
+++ src/main/java/com/zimo/springbootstu/SpringbootStuApplication.java
@@ -0,0 +1,13 @@
+package com.zimo.springbootstu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zimo/springbootstu/SpringbootStuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zimo.springbootstu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
